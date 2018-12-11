07b05c9f89cbdd192ccf420a9e2f9ec3336b7a8f
dan <dan@danjaboor.com>
2018-12-10 18:15:47 -0800

    Final Version

--- src/TechJobsConsole/JobData.cs
+++ src/TechJobsConsole/JobData.cs
@@ -57,7 +57,25 @@ namespace TechJobsConsole
 
             return jobs;
         }
+        public static List<Dictionary<string, string>> FindByValue(string value)
+        {
+            LoadData();
 
+            List<Dictionary<string, string>> jobs = new List<Dictionary<string, string>>();
+
+            foreach (Dictionary<string, string> row in AllJobs)
+            {
+                foreach (string Info in row.Values)
+                {
+                    if (Info.ToLower().Contains(value.ToLower()))
+                    {
+                        jobs.Add(row);
+                        break;
+                    }
+                }
+            }
+            return jobs;
+        }
         /*
          * Load and parse data from job_data.csv
          */
